4edb3da9448b9b5ab543d636e5ed206d12de4aa2
marlonzl7 <marlon.silva@sptech.school>
2025-11-16 03:17:45 -0300

    implementado frontend para cadastro de funcionários com códigod e segurança

---
+++ entregaveis-grupo/site/public/js/site-institucional/cadastrar-funcionario.js
@@ -0,0 +1,40 @@
+function cadastrar(nome, sobrenome, email, senha, codigoSeguranca) {
+    const nomeValido = validarNome(nome);
+    const sobrenomeValido = validarSobrenome(sobrenome);
+    const emailValido = validarEmail(email);
+    const senhaValida = validarSenha(senha);
+
+    if (!nomeValido || !sobrenomeValido || !emailValido || !senhaValida) {
+        alert("Formulário com erros, corrija antes de enviar.");
+        return;
+    }
+
+    const dados = {
+        nome: nome,
+        sobrenome: sobrenome,
+        email: email,
+        senha: senha,
+        codigoSeguranca: codigoSeguranca
+    };
+
+    fetch("/usuarios/cadastrar-com-codigo", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dados)
+    }) 
+    .then (function (resposta) {
+        if (!resposta.ok) {
+            throw "Erro ao cadastrar funcionário.";
+        }
+
+        resposta.json().then(json => {
+            alert("Cadastro realizado com sucesso!");
+        })
+    })
+    .catch (function (erro) {
+        console.error("Erro: ", erro);
+        alert("Ocorreu um erro ao cadastrar. Tente novamente.");
+    });
+}
--- entregaveis-grupo/site/public/js/site-institucional/validacoes.js
+++ entregaveis-grupo/site/public/js/site-institucional/validacoes.js
@@ -1,3 +1,90 @@
+function validarNome(input) {
+    errNome.innerHTML = "";
+    const tamanhoMinimo = 2;
+
+    if (input.length < tamanhoMinimo) {
+        errNome.innerHTML = `<b style="font-size: 9px; color: red;" ">NOME DEVE CONTER NO MÍNIMO 3 LETRAS</b> <br>`;
+    }
+
+    for (let i = 0; i < input.length; i++) {
+        const codigo_ascii = input.charCodeAt(i);
+
+        if (!((codigo_ascii >= 65 && codigo_ascii <= 90) || (codigo_ascii >= 97 && codigo_ascii <= 122) || (codigo_ascii === 32))) {
+            errNome.innerHTML = `<b style="font-size: 9px; color: red;" ">NOME SÓ DEVE CONTER LETRAS</b> <br>`;
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function validarSobrenome(input) {
+    errSobrenome.innerHTML = "";
+    const tamanhoMinimo = 2;
+
+    if (input.length < tamanhoMinimo) {
+        errSobrenome.innerHTML = `<b style="font-size: 9px; color: red;" ">SOBRENOME DEVE CONTER NO MÍNIMO 3 LETRAS</b> <br>`;
+    }
+
+    for (let i = 0; i < input.length; i++) {
+        const codigo_ascii = input.charCodeAt(i);
+
+        if (!((codigo_ascii >= 65 && codigo_ascii <= 90) || (codigo_ascii >= 97 && codigo_ascii <= 122) || (codigo_ascii === 32))) {
+            errSobrenome.innerHTML = `<b style="font-size: 9px; color: red;" ">SOBRENOME SÓ DEVE CONTER LETRAS</b> <br>`;
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function validarSenha(input) {
+    let contemMinuscula = false;
+    let contemMaiuscula = false;
+    let contemNumero = false;
+    let contemEspecial = false;
+    let tamanhoMinimo = false;
+
+    if (input.length >= 8) {
+        tamanhoMinimo = true;
+    }
+
+    for (let i = 0; i < input.length; i++) {
+        const codigo_ascii = input.charCodeAt(i);
+
+        if (codigo_ascii >= 97 && codigo_ascii <= 122) {
+            contemMinuscula = true;
+        } else if (codigo_ascii >= 65 && codigo_ascii <= 90) {
+            contemMaiuscula = true;
+        } else if (codigo_ascii >= 48 && codigo_ascii <= 57) {
+            contemNumero = true;
+        } else if (
+            (codigo_ascii >= 32 && codigo_ascii <= 47) || 
+            (codigo_ascii >= 58 && codigo_ascii <= 64) || 
+            (codigo_ascii >= 91 && codigo_ascii <= 96) || 
+            (codigo_ascii >= 123 && codigo_ascii <= 126)) 
+        {
+            contemEspecial = true;
+        }
+    }
+
+    let msg = "";
+
+    msg += (!tamanhoMinimo) ? `<b style="font-size: 9px; color: red;" ">SENHA DEVE TER NO MÍNIMO 8 CARACTERES.</b> <br>` : "";
+    msg += (!contemMinuscula) ? `<b style="font-size: 9px; color: red;" ">SENHA DEVE TER PELO MENOS UMA LETRA MINÚSCULA.</b> <br>` : "";
+    msg += (!contemMaiuscula) ? `<b style="font-size: 9px; color: red;" ">SENHA DEVE TER PELO MENOS UMA LETRA MAIÚSCULA.</b> <br>` : "";
+    msg += (!contemNumero) ? `<b style="font-size: 9px; color: red;" ">SENHA DEVE TER PELO MENOS UM NÚMERO.</b> <br>` : "";
+    msg += (!contemEspecial) ? `<b style="font-size: 9px; color: red;" ">SENHA DEVE TER PELO MENOS UM CARACTERE ESPECIAL.</b>` : "";
+
+    if (msg != "") {
+        errSenha.innerHTML = msg;
+        return false;
+    } else {
+        errSenha.innerHTML = "";
+        return true;
+    }
+}
+
 function validarCNPJ(input) {
     errCNPJ.innerHTML = "";
     const tamanhoMinimo = input.length == 14;
