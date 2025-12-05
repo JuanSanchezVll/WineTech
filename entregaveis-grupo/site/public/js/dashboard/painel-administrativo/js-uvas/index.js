04cc37aac203f01170b450f20038d5b126695ca6
marlonzl7 <marlon.silva@sptech.school>
2025-12-05 06:47:12 -0300

    implementado listagem e pesquisa de uvas na página de gerenciar uvas

---
+++ entregaveis-grupo/site/public/js/dashboard/painel-administrativo/js-uvas/index.js
@@ -0,0 +1,46 @@
+async function carregarUvas() {
+    const uvas = await obterUvas();
+    exibirTabela(uvas);
+}
+
+async function pesquisarUvas(pesquisa) {
+    const resultado = await fetch(`/uvas/pesquisar?idEmpresa=${sessionStorage.ID_EMPRESA}&pesquisa=${pesquisa}`);
+    const uvas = await resultado.json();
+
+    exibirTabela(uvas);
+}
+
+async function obterUvas() {
+    const resultado = await fetch(`/uvas/listar?idEmpresa=${sessionStorage.ID_EMPRESA}`);
+    return await resultado.json();
+}
+
+async function exibirTabela(dados) {
+    const tabela = document.getElementById("conteudo-tabela-uva");
+
+    tabela.innerHTML = "";
+
+    if (!dados || dados.lenght === 0) {
+        tabela.innerHTML = "Nenhuma uva encontrada";
+        return;
+    }
+
+    for (let i = 0; i < dados.length; i++) {
+        tabela.innerHTML += `
+            <tr>
+                <td>${dados[i].id_uva}</td>
+                <td>${dados[i].nome}</td>
+                <td>${dados[i].temperatura_minima}</td>
+                <td>${dados[i].temperatura_maxima}</td>
+                <td>${dados[i].umidade_minima}</td>
+                <td>${dados[i].umidade_maxima}</td>
+                <td><a href="./atualizar.html">
+                    <img src="./../../../assets/icones/painel-administrativo/editar-icon.svg" class="action-icon"></a>
+                </td>
+                <td>
+                    <img src="./../../../assets/icones/painel-administrativo/delete-icon.svg" class="action-icon">
+                </td>
+            </tr>
+        `;
+    }
+}
