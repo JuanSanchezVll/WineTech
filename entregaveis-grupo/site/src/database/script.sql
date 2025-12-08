d124ca65fbcc864f2b6cc4320f866acbc6a699cf
0tavio4ugusto <otavio.rodrigues@sptech.school>
2025-12-08 15:11:51 -0300

    arrumei coisas de cave e o script de novo

--- entregaveis-grupo/site/src/database/script.sql
+++ entregaveis-grupo/site/src/database/script.sql
@@ -345,7 +345,7 @@ CREATE VIEW vw_alertas_por_cave AS
 	JOIN cave c ON b.id_cave = c.id_cave
     WHERE
 		MONTH(a.data_hora_registro) = MONTH(CURRENT_DATE())
-        AND YEAR(a.data_hora_registro) = YEAR(CURRENT_DATA())
+        AND YEAR(a.data_hora_registro) = YEAR(CURRENT_DATE())
 	GROUP BY 
 		c.id_empresa,
 		c.id_cave,
