8ef4e688dce00d836dae7a7d451c00c19ef95551
RafaelSouza115 <Rafael.asantos@Sptech.school>
2025-12-08 15:26:55 -0300

    Func: Kpis da dashboard com funcionalidades

--- entregaveis-grupo/site/src/models/dashboardIndexModel.js
+++ entregaveis-grupo/site/src/models/dashboardIndexModel.js
@@ -44,8 +44,46 @@ async function obterUltimaLeituraNoBarril(idEmpresa, idCave, idBarril) {
     return await database.execute(instrucao);
 }
 
+async function obterCaveComAlertaTemperatura(idEmpresa) {
+    const instrucao = `
+        SELECT 
+            CONCAT(c.identificacao, ' - ', b.identificacao) as local_alerta
+        FROM alerta a
+        JOIN leitura l ON a.id_leitura = l.id_leitura
+        JOIN sensor s ON l.id_sensor = s.id_sensor
+        JOIN barril b ON s.id_barril = b.id_barril
+        JOIN cave c ON b.id_cave = c.id_cave
+        JOIN uva u ON b.id_uva = u.id_uva
+        WHERE c.id_empresa = ${idEmpresa}
+          AND (l.temperatura < u.temperatura_minima OR l.temperatura > u.temperatura_maxima)
+        ORDER BY a.data_hora_registro DESC
+        LIMIT 1;
+    `;
+    return await database.execute(instrucao);
+}
+
+async function obterCaveComAlertaUmidade(idEmpresa) {
+    const instrucao = `
+        SELECT 
+            CONCAT(c.identificacao, ' - ', b.identificacao) as local_alerta
+        FROM alerta a
+        JOIN leitura l ON a.id_leitura = l.id_leitura
+        JOIN sensor s ON l.id_sensor = s.id_sensor
+        JOIN barril b ON s.id_barril = b.id_barril
+        JOIN cave c ON b.id_cave = c.id_cave
+        JOIN uva u ON b.id_uva = u.id_uva
+        WHERE c.id_empresa = ${idEmpresa}
+          AND (l.umidade < u.umidade_minima OR l.umidade > u.umidade_maxima)
+        ORDER BY a.data_hora_registro DESC
+        LIMIT 1;
+    `;
+    return await database.execute(instrucao);
+}
+
 module.exports = {
     listarAlertasPorCaveNoMes,
     obterTotalAlertasNoMes,
-    obterUltimaLeituraNoBarril
+    obterUltimaLeituraNoBarril,
+    obterCaveComAlertaTemperatura,
+    obterCaveComAlertaUmidade
 }
--- entregaveis-grupo/site/src/routes/dashboardIndex.js
+++ entregaveis-grupo/site/src/routes/dashboardIndex.js
@@ -15,4 +15,15 @@ router.get("/ultimaLeitura/empresa/:idEmpresa/caves/:idCave/barris/:idBarril", f
     dashboardIndexController.obterUltimaLeituraNoBarril(req, res);
 })
 
+
+router.get("/alertas/temperatura/empresa/:idEmpresa", function(req, res) {
+    dashboardIndexController.obterCaveComAlertaTemperatura(req, res);
+});
+
+router.get("/alertas/umidade/empresa/:idEmpresa", function(req, res) {
+    dashboardIndexController.obterCaveComAlertaUmidade(req, res);
+});
+
+module.exports = router;
+
 module.exports = router;
